d47619ba498cd73633641691635624132dcd19b9
Mills24 <55164462+Mills24@users.noreply.github.com>
2019-11-11 18:48:05 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const chalk = require('chalk');
+const log = console.log;
+
+log(chalk.keyword('purple')('When cares threaten to overtake you'));
+log(chalk.blue('And you’re waiting for the sky to clear,'));
+log(chalk.green('Remember that life is a series of ') +
+chalk.blue.underline.bold('changes') + chalk.green(','));
+log(chalk.yellow('And a ') + chalk.bold.white('brighter') + chalk.yellow(' tomorrow is always near.'));
+
+log(chalk.keyword('orange')('Each day is a new beginning'));
+log(chalk.red('That brings us a beautiful dawn.'));
+log(chalk.yellow('Each time you feel you have come'));
+log(chalk.blue('To the end of your rope,'));
+log(chalk.green('Tie a knot into and hang on.'));
+
+log(chalk.keyword('purple')('May your whole being be'));
+log(chalk.red.bgWhiteBright('Filled with hope, faith and trust'));
+log(chalk.blue('That God and the Angels are part'));
+log(chalk.keyword('orange')('Of every human being and'));
+log(chalk.green('Therefore also of you.'));
+log(chalk.yellow('They have only your best at heart'));
+log(chalk.green('And will never leave you.'));
+
+//Used a poem form https://www.shortstories101.com/story/never-give-up/
+
